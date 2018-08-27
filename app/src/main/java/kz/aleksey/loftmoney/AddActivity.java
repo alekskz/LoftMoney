26c60c018440a6a119d8c208dc188b1fbb043718
Aleksey Kozhemyakin <aleks.kzh@gmail.com>
2018-08-27 18:02:29 +0300

    Create res/color. Create drawable/ic_check. Added Button.

--- app/src/main/java/kz/aleksey/loftmoney/AddActivity.java
+++ app/src/main/java/kz/aleksey/loftmoney/AddActivity.java
@@ -2,12 +2,38 @@ package kz.aleksey.loftmoney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+    private EditText nameInput;
+    private EditText priceInput;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        nameInput = findViewById(R.id.name);
+        priceInput = findViewById(R.id.price);
+        addBtn = findViewById(R.id.add_btn);
+
+       // private TextWatcher textWatcher = new TextWatcher() {
+       //     @Override
+       //     public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+       //     @Override
+       //     public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+       //     @Override
+       //     public void afterTextChanged(Editable s) {
+       //         addBtn.setEnabled(!TextUtils.isEmpty(s));
+       //     }
+       // };
+        // nameInput.addTextChangedListener(textWatcher);
+        //priceInput.addTextChangedListener(textWatcher);
+
     }
 }
