befc71c5dc43af1b833ec4f7b1f405c2dfa3a2b1
Aleksey Kozhemyakin <aleks.kzh@gmail.com>
2018-08-27 19:30:01 +0300

    Added class EditTextListener in AddActivity.

--- app/src/main/java/kz/aleksey/loftmoney/AddActivity.java
+++ app/src/main/java/kz/aleksey/loftmoney/AddActivity.java
@@ -2,6 +2,9 @@ package kz.aleksey.loftmoney;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,20 +23,20 @@ public class AddActivity extends AppCompatActivity {
         priceInput = findViewById(R.id.price);
         addBtn = findViewById(R.id.add_btn);
 
-       // private TextWatcher textWatcher = new TextWatcher() {
-       //     @Override
-       //     public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-       //     @Override
-       //     public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-       //     @Override
-       //     public void afterTextChanged(Editable s) {
-       //         addBtn.setEnabled(!TextUtils.isEmpty(s));
-       //     }
-       // };
-        // nameInput.addTextChangedListener(textWatcher);
-        //priceInput.addTextChangedListener(textWatcher);
-
+        nameInput.addTextChangedListener(new EditTextListerm());
     }
+        private class EditTextListerm implements TextWatcher {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                addBtn.setEnabled(!TextUtils.isEmpty(s));
+            }
+        }
 }
+
